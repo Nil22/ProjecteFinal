a8b054214331a4556620c301559af8963e66e37d
lop1381997 <ogilop@gmail.com>
2017-05-17 11:21:03 +0200

    Lcs_main

---
+++ LCS_main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+/**
+ * Created by ogilo on 17/05/2017.
+ */
+public class LCS_main {
+    public static void main(){
+        Scanner first=new Scanner(System.in);
+        Scanner second=new Scanner(System.in);
+    }
+}
