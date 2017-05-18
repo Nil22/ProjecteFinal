cbd2d7730cc941b910359ed31b2ea42de3e3916b
lop1381997 <ogilop@gmail.com>
2017-05-18 19:19:21 +0200

    creacio del main i modificacio de la clase LCS per poder executar-ho

--- LCS.java
+++ LCS.java
@@ -5,18 +5,37 @@ import java.util.*;
  */
 public class LCS {
     // default arguments on i,j equale to 0
-
+    public char b[];
+    public char c [];
+    public LCS(){
+        Scanner first=new Scanner(System.in);
+        Scanner second=new Scanner(System.in);
+        this.b=new char[1000000];
+        this.c=new char[1000000];
+        int i=0;
+        while(first.hasNext()){
+            b[i]=first.next().charAt(0);
+            ++i;
+        }
+        i=0;
+        while(second.hasNext()){
+            c[i]=second.next().charAt(0);
+            ++i;
+        }
+        int [][] result = new int [b.length][c.length];
+        result=this.LCS(b,c,0,0);
+    }
     public int [][] LCS(char b [], char c[], int i, int j){
-        int [][] result =new int [b.length()][c.length()];
-        int [][] result_aux =new int [b.length()][c.length()];
-        for (int k = 0; k <b.length() ; i++) {
+        int [][] result =new int [b.length][c.length];
+        int [][] result_aux =new int [b.length][c.length];
+        for (int k = 0; k <b.length ; i++) {
             result[k][0]=0;
         }
-        for (int k = 0; k <c.length() ; i++) {
+        for (int k = 0; k <c.length ; i++) {
             result[0][k]=0;
         }
-        while(i<b.length()){
-            while(j<c.length()){
+        while(i<b.length){
+            while(j<c.length){
                 if(b[i]==c[j]){
                     result[i][j]=1;
                 }
@@ -26,8 +45,8 @@ public class LCS {
                 }
             }
         }
-        for(int i=1;i<b.length();++i){
-            for (int k = 0; k <c.length() ; k++) {
+        for(int l=1;l<b.length;++i){
+            for (int k = 0; k <c.length ; k++) {
                 if(result_aux[i][j]==1){
                     result[i][j]=1;
                 }
--- LCS_main.java
+++ LCS_main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 /**
  * Created by ogilo on 17/05/2017.
  */
 public class LCS_main {
-    public static void main(){
-        Scanner first=new Scanner(System.in);
-        Scanner second=new Scanner(System.in);
+    public static  void main(String[] args){
+      LCS a=new LCS();
     }
-}
+  }
